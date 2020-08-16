76815e5d93130425da67646acf739c5eb0a57eff
Fran Zekan <zekan.fran369@gmail.com>
2020-08-16 18:22:40 +0200

    Make generic scripts() function that fallsback from jq to tail and sed

--- functions.sh
+++ functions.sh
@@ -1,5 +1,27 @@
+#!/bin/sh
+
+# Sometimes PSQL craps all over itself and you can't restart it, use this instead
 function rpsql() {
   brew services stop postgresql
-  rm /usr/local/var/postgres/postmaster.pid # adjust path accordingly to your install
+  rm /usr/local/var/postgres/postmaster.pid
   brew services start postgresql
+}
+
+# List yarn scripts from package.json
+function scripts() {
+  FILE=./package.json
+  if [ ! -f "$FILE" ]; then
+    echo "Can't find package.json"
+    return 1
+  fi
+
+  if ! command -v jq &> /dev/null; then
+    FIRST_LINE=$(cat $FILE | grep -n "\"scripts\": {" | cut -f1 -d:)
+    OFFSET=$(tail -n +$FIRST_LINE $FILE | grep -n "}" | head -n 1 | cut -f1 -d:)
+    LAST_LINE="$(($FIRST_LINE + $OFFSET - 2))"
+
+    sed -n "$(($FIRST_LINE +1)),$LAST_LINE"p $FILE | awk '$1=$1'
+  else
+    cat $FILE | jq '{scripts} | .["scripts"]'
+  fi
 }
