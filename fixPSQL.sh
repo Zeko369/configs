ab6d01e26bee51be29f410c67a9dc6743e88130e
Fran Zekan <zekan.fran369@gmail.com>
2020-08-16 18:27:20 +0200

    Remove unsued stuff

--- fixPSQL.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# https://dba.stackexchange.com/questions/75214/postgresql-not-running-on-mac
-
-brew services stop postgresql
-rm /usr/local/var/postgres/postmaster.pid # adjust path accordingly to your install
-brew services start postgresql
--- functions.sh
+++ functions.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Sometimes PSQL craps all over itself and you can't restart it, use this instead
+# https://dba.stackexchange.com/questions/75214/postgresql-not-running-on-mac
 function rpsql() {
   brew services stop postgresql
   rm /usr/local/var/postgres/postmaster.pid
