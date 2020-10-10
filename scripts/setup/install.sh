4267fd6fc0e826e6e15920b73abd2de996666968
Fran Zekan <zekan.fran369@gmail.com>
2020-10-10 23:15:29 +0200

    Add fonts and some other stuff

--- scripts/setup/install.sh
+++ scripts/setup/install.sh
@@ -9,12 +9,16 @@ sudo apt install curl wget nmap -y
 # Utils
 sudo apt install tmux zsh git -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Vim
 sudo apt install vim vim-gtk -y
 
 # NVM
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh)"
+
+# Fonts
+sudo apt install fonts-firacode
 
 # RBENV
 sudo apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
@@ -23,8 +27,8 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
 # Yarn
@@ -32,10 +36,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
-sudo apt install yarn -y
-
-# Others
-sudo apt install xclip
+sudo apt update && sudo apt install --no-install-recommends yarn
 
 # Gen ssh keys
 # ssh-keygen -b 2048 -t rsa -f foo123 -q -N ""
@@ -72,3 +73,8 @@ sudo apt install fonts-firacode -y
 # Mysql, psql, mongo?
 
 # drivers for gpu
+
+# Setup keyboard layouts
+
+# setup watchers
+# https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc 
