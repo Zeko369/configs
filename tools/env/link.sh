697fe9c5095642e6af2c72dc4d7dbba4c8d3b03b
Fran Zekan <zekan.fran369@gmail.com>
2020-10-11 22:34:44 +0200

    Fix link script

--- tools/env/link.sh
+++ tools/env/link.sh
@@ -3,7 +3,7 @@
 [ -f ~/.vimrc ] && cp ~/.vimrc ~/.vimrc.bak
 [ -f ~/.tmux.conf ] && cp ~/.tmux.conf ~/.tmux.conf.bak
 
-ln -fs ~/repos/configs/vimrc ~/.vimrc
+ln -fs ~/repos/configs/vim/basic_vimrc ~/.vimrc
 ln -fs ~/repos/configs/tmux.conf ~/.tmux.conf
 
 [ -f ~/repos/configs/shell/zshrc.base.local ] && cp ~/repos/configs/shell/zshrc.base.local ~/repos/configs/shell/zshrc.base.local.bak
