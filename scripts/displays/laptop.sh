1bf69b40d30932ecf6fbf8bfb64940ce11820f52
Fran Zekan <zekan.fran369@gmail.com>
2021-12-24 12:09:49 +0100

    Laptop/SideLaptop scripts

---
+++ scripts/displays/laptop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+displayplacer "id:2A9F7159-CE93-954E-0857-D6964E3302DB res:3840x1600 hz:120 color_depth:7 scaling:off origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1280x800 hz:60 color_depth:8 scaling:on origin:(1236,1600) degree:0"
