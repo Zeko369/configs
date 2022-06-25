a9c7e92549fe7391ec1762ce01b268d3c9ea9bfa
Fran Zekan <zekan.fran369@gmail.com>
2022-06-25 20:02:09 +0200

    Add Raycast scripts for display management

---
+++ scripts/displays/raycast/displays-laptop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title displays laptop
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 💻
+# @raycast.packageName Display management
+
+../laptop.sh
