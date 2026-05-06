b9257ebbd3be11858cc4ce6ae1d07fcd5a19d9aa
Fran Zekan <zekan.fran369@gmail.com>
2026-05-06 20:31:30 +0200

    remove legacy shit

--- shell/ls.sh
+++ shell/ls.sh
@@ -10,15 +10,3 @@ alias la='eza -la --git --icons'
 # Tree view
 alias lt='eza --tree --level=2 --icons --git -I "node_modules|.git|vendor"'
 
-# Toggle git info for performance in large repos
-function lsToggleGit() {
-  if [ -n "$NO_GIT" ]; then
-    unset NO_GIT
-    echo "Git info enabled in ls"
-  else
-    export NO_GIT=true
-    echo "Git info disabled in ls"
-  fi
-}
-
-alias ltg="lsToggleGit"
